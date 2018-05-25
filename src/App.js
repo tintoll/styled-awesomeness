c1ad26750f8de2e1149a56cdb786315f12474dd7
tintoll <belselios@gmail.com>
2018-05-26 00:40:36 +0900

    init setting

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <button>Hello</button>
+        <button>Hello</button>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
